da0db76951ce6b09f24684099c4c235e665663e7
Vojteu <wojtasss821@gmail.com>
2023-10-13 18:51:44 +0200

    update

--- src/main/java/vojteu/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/vojteu/springframework/Spring5RecipeAppApplication.java
@@ -2,6 +2,11 @@ package vojteu.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import vojteu.springframework.domain.Category;
+import vojteu.springframework.repositories.CategoryRepository;
+
+
 
 @SpringBootApplication
 public class Spring5RecipeAppApplication {

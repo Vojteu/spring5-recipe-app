8212c1bef8b9b835ca9bd0ace922636da9b4eaf7
Vojteu <wojtasss821@gmail.com>
2023-11-16 18:46:50 +0100

    update

---
+++ src/test/java/vojteu/springframework/controllers/IngredientControllerTest.java
@@ -0,0 +1,9 @@
+package vojteu.springframework.controllers;
+
+import org.mockito.Mock;
+import vojteu.springframework.services.RecipeService;
+
+public class IngredientControllerTest {
+    @Mock
+    RecipeService recipeService;
+}

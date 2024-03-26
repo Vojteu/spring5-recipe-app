ba52a45acccf0cb6e20a877e8165993bc3c3445c
Vojteu <wojtasss821@gmail.com>
2024-03-26 15:34:30 +0100

    update

--- src/main/java/vojteu/springframework/services/IngredientServiceImpl.java
+++ src/main/java/vojteu/springframework/services/IngredientServiceImpl.java
@@ -9,36 +9,36 @@ import vojteu.springframework.repositories.RecipeRepository;
 
 import java.util.Optional;
 
-@Slf4j
-@Service
-public class IngredientServiceImpl implements IngredientService {
+    @Slf4j
+    @Service
+    public class IngredientServiceImpl implements IngredientService {
 
-    private final IngredientToIngredientCommand ingredientToIngredientCommand;
-    private final RecipeRepository recipeRepository;
+        private final IngredientToIngredientCommand ingredientToIngredientCommand;
+        private final RecipeRepository recipeRepository;
 
-    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository) {
-        this.ingredientToIngredientCommand = ingredientToIngredientCommand;
-        this.recipeRepository = recipeRepository;
-    }
-
-    @Override
-    public IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId) {
-        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
-
-        if(!recipeOptional.isPresent()){
-            log.error("recipe id not found. Id: " + recipeId);
+        public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository) {
+            this.ingredientToIngredientCommand = ingredientToIngredientCommand;
+            this.recipeRepository = recipeRepository;
         }
 
-        Recipe recipe = recipeOptional.get();
+        @Override
+        public IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId) {
+            Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
 
-        Optional<IngredientCommand> ingredientCommandOptional = recipe.getIngredients().stream()
-                .filter(ingredient -> ingredient.getId().equals(ingredientId))
-                .map(ingredient -> ingredientToIngredientCommand.convert(ingredient)).findFirst();
+            if(!recipeOptional.isPresent()){
+                log.error("recipe id not found. Id: " + recipeId);
+            }
 
-        if(!ingredientCommandOptional.isPresent()){
-            log.error("Ingredient id not found: " + ingredientId);
+            Recipe recipe = recipeOptional.get();
+
+            Optional<IngredientCommand> ingredientCommandOptional = recipe.getIngredients().stream()
+                    .filter(ingredient -> ingredient.getId().equals(ingredientId))
+                    .map(ingredient -> ingredientToIngredientCommand.convert(ingredient)).findFirst();
+
+            if(!ingredientCommandOptional.isPresent()){
+                log.error("Ingredient id not found: " + ingredientId);
+            }
+
+            return ingredientCommandOptional.get();
         }
-
-        return ingredientCommandOptional.get();
     }
-}
